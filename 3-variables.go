4abbabdd245d98a77f894e4512ff9fe89284d2a1
borisdvlpr <eduffmiranda@hotmail.com>
2024-07-04 20:43:10 +0200

    added variables snippet

---
+++ 3-variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c = 1, 2
+	fmt.Println(b, c)
+
+	var d, e = 7.0, 7.1
+	fmt.Println(d, e)
+
+	var f = true
+	fmt.Println(f)
+
+	var g int
+	fmt.Println(g)
+
+	h := "linux"
+	fmt.Println(h)
+}
